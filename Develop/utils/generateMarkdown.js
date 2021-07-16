0acfd7171d13833fec278409a771fdbf601c44f9
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-07-16 11:42:38 -0700

    generatemarkdown funcion

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -11,10 +11,64 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const generateMarkdown = data => {
+  return `
+  
+**${data.title}**
 
+ ![Github Badge] (https://img.shields.io/badge/Badges-${data.badge}-green)
+
+## Description 
+
+${data.description}
+
+## Table of contents
+
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Licence](#Licence)
+- [Contributors](#Contributors)
+- [Test](#tests)
+- [Repository Link](#Repository)
+- [GitHub Info](#GitHub) 
+- [Questions](#questions)
+
+
+## Installation
+
+  ${data.install}
+
+## Usage
+
+${data.usage}
+
+## Licence
+
+![Github licence](http://img.shields.io/badge/license-${data.license}-red.svg)
+
+## Contributors
+
+${data.contributors}
+
+## Test
+
+${data.test}
+
+
+## Repository
+
+- [Project Repo](${data.repo})
+
+## GitHub
+
+- [GitHub Profile](${githubInfo.profile})
+## Questions
+  If you have any questions about this projects, please contact me directly at ${githubInfo.email}. 
+  You can view more of my projects at https://github.com/${data.github}.
 `;
 }
 
+
 module.exports = generateMarkdown;
+
