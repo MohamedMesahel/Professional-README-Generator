8c7f6dae211ff02087b89a93e52daa8a5b29afb4
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-07-16 13:47:25 -0700

    License Badges

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -8,8 +8,29 @@
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-// function renderLicenseSection(license) {}
-
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "Apache License 2.0":
+      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]";
+    case "BSD 3 license":
+      return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]";
+    case "BSD 2 license":
+      return "[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)]";
+    case "GNU GPL V3":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]"
+    case "GNU GPL V2":
+      return "[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)]"
+   
+    case "IBM Public License Version 1.0":
+      return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)]";
+    case "MIT License":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
+    case "Mozilla Public License 2.0":
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)]";
+    default:
+      return "";
+  }
+}
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = data => {
   return `
