9d48018281a42acb8f32e87aa2880c9e324c2d18
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-07-16 18:13:18 -0700

    license

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -58,18 +58,35 @@
 //       return `${renderLicenseBadge(license)}${renderLicenseLink(license)} \n\n`;
 //   }
 // }
+function licenseBadge(data) {
+  const licenseType = data.license[0];
+  let licenseString = " "
+  if (licenseType === "MIT") {
+    licenseString = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  };
+  if (licenseType === "GNU General Public License 2.0") {
+    licenseString = `![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)`
+  };
+  if (licenseType === "Apache License 2.0") {
+    licenseString = `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`
+  };
+  if (licenseType === "GNU General Public License 3.0") {
+    licenseString = `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+  };
+  return licenseString
+};
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = data => {
-  switch (data.license) {
-    case "Apache License 2.0":
-      licenseBadge = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
-      break;
-    case "BSD 3 license":
-      licenseBadge = `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
-      break;
-    default:
+  // switch (data.license) {
+  //   case "Apache License 2.0":
+  //     licenseBadge = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  //     break;
+  //   case "BSD 3 license":
+  //     licenseBadge = `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+  //     break;
+  //   default:
 
-    }
+  //   }
   return `
   
 **${data.title}**
@@ -103,7 +120,7 @@ ${data.usage}
 
 ## License
  
-This project is licensed under ${data.license}
+This project is licensed under ${licenseBadge(data)}
 
 ## Contributors
 
